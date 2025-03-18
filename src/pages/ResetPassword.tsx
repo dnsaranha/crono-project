47773237f9865ec358c692523cd44d2d4e0f2b7d
dnsaranha <58961682+dnsaranha@users.noreply.github.com>
2025-03-18 19:29:33 -0300

    Atualizar o ResetPassword.tsx

--- src/pages/ResetPassword.tsx
+++ src/pages/ResetPassword.tsx
@@ -20,9 +20,8 @@ export default function ResetPassword() {
       setLoading(true);
       
       const { error } = await supabase.auth.resetPasswordForEmail(email, {
-        redirectTo: `${window.location.origin}/update-password`,
+        redirectTo: `${window.location.origin}/auth/update-password`,
       });
-
       if (error) throw error;
       
       setSent(true);
